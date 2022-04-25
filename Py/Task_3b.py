f43b69ccd68683a24fa62bc5cba2b57c125eaa25
Egor Tkachenko <tkachenko.egor.a@gmail.com>
2022-04-26 02:47:44 +0300

    py

--- Py/Task_3b.py
+++ Py/Task_3b.py
@@ -5,17 +5,16 @@ import math
 import random
 
 def isLeftTurn(points):
-    return(np.cross(np.subtract(points[1], points[0]), np.subtract(points[2], points[1]))>0)
+    return(np.cross(points[1] - points[0], points[2] - points[1]) > 0)
 
 def isInTriangle(triangle, points):
     for point in points:
-        s1 = (triangle[1][0] - point[0]) * (triangle[2][1] - triangle[1][1]) - (triangle[2][0] - triangle[1][0]) * (triangle[1][1] - point[1])
-        s2 = (triangle[2][0] - point[0]) * (triangle[0][1] - triangle[2][1]) - (triangle[0][0] - triangle[2][0]) * (triangle[2][1] - point[1])
-        s3 = (triangle[0][0] - point[0]) * (triangle[1][1] - triangle[0][1]) - (triangle[1][0] - triangle[0][0]) * (triangle[0][1] - point[1])
-        if (s1 > 0 and s2 > 0 and s3 > 0) or (s1 < 0 and s2 < 0 and s3 < 0):
+        s1 = isLeftTurn([triangle[0], triangle[1], point])
+        s2 = isLeftTurn([triangle[1], triangle[2], point])
+        s3 = isLeftTurn([triangle[2], triangle[0], point])
+        if (s1 == s2 and s2 == s3):
             return(True)
     return(False)
-    
 
 def triangulate(polygon):
     triangles = list()
@@ -38,48 +37,31 @@ def plotPolygon(polygon):
     plt.plot(pol_x, pol_y)
 
 def area(triangle):
-    return(abs(np.cross(np.subtract(triangle[1], triangle[0]), np.subtract(triangle[2], triangle[0])))/2)
+    return(abs(np.cross(triangle[1] - triangle[0], triangle[2] - triangle[0])) / 2)
 
 def TriangleRandom(triangle):
     a = random.uniform(0, 1)
     b = random.uniform(0, 1)
-    if a+b>1:
-        a, b = 1-a, 1-b
-    return(np.add(triangle[0], np.add(a*np.subtract(triangle[1], triangle[0]), b * np.subtract(triangle[2], triangle[0]))))
+    if a + b > 1:
+        a, b = 1 - a, 1 - b
+    return((1-a-b) * triangle[0] + a * triangle[1] + b * triangle[2])
 
 
 
 
-polygon = [list(map(float, line.split(","))) for line in fileinput.input(files ='polygon.txt')]
-
-
-
+polygon = [np.array(list(map(float, line.split(",")))) for line in fileinput.input(files ='Py/polygon.txt')]
 triangles = triangulate(polygon)
 
+areas = np.array(list(map(area, triangles)))
+areas = areas / sum(areas)
 
-areas = [area(triangle) for triangle in triangles]
+randomPoints = [TriangleRandom(triangles[k]) for k in np.random.choice(len(triangles), 10000, p=areas)]
 
-areassum = sum(areas)
-
-areas = [area/areassum for area in areas]
-
-
-randomPoints = [TriangleRandom(triangles[k]) for k in np.random.choice([i for i in range(len(triangles))], 1000, p=areas)]
-
-
-
-
-
-
-plotPolygon(polygon)
 
 for triangle in triangles:
     plotPolygon(triangle)
-# plotPolygon(triangles[0])
 
 x, y = zip(*randomPoints)
-
-# plt.plot(x, y, 'o')
+plt.scatter(x, y, marker='o', s=1)
 
 plt.show()
-
