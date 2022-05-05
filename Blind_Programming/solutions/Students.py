830d9291a375ac88ad92186f0969de85523a36b8
Egor Tkachenko <tkachenko.egor.a@gmail.com>
2022-05-06 00:21:22 +0300

    .

--- Blind_Programming/solutions/Students.py
+++
@@ -1,3 +0,0 @@
-from math import floor
-n = int(input())
-print(-1 if (n % 2 == 1 or n < 4) else floor((n * 3 - 2) / 4))
--- Blind_Programming/solutions/test.py
+++
@@ -1,4 +0,0 @@
-from random import randrange
-
-for i in range(100):
-    print(randrange(-100, 100))
