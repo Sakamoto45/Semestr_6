b21fc5782d11ec3b7b3fb647d2177cc0fc571bf8
Egor Tkachenko <tkachenko.egor.a@gmail.com>
2022-05-29 22:32:21 +0300

    c++

---
+++ C++/HypothesisTestingExample/main.cpp
@@ -0,0 +1,14 @@
+#include "document.h"
+
+int main() {
+    Document doc = Document();
+
+    doc.set_distribution_0(0.2, 10);
+    doc.set_distribution_1(0.2, 10, 1000);
+    
+
+
+
+
+    return 0;
+}
